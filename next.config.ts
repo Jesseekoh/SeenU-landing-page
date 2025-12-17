fcecff49f8faf0a0dfa7b9006cfe9254d3c0c142
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-12-17 11:17:32 +0100

    feat: Add core UI dependencies, component configuration, utility functions, and static assets.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        pathname: "/**", // Optional, specifies the image path pattern
+      },
+      {
+        protocol: "https",
+        hostname: "bumble.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
