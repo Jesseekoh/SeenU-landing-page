4559a0b3931361fb71894741efb3ca4e7b2bf99a
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-12-17 18:14:44 +0100

    feat: Add reviews carousel, refine navbar scrolling and mobile menu, and update legal page layouts.

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-        pathname: "/**", // Optional, specifies the image path pattern
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        pathname: '/**', // Optional, specifies the image path pattern
       },
       {
-        protocol: "https",
-        hostname: "bumble.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.cdninstagram.com',
+        pathname: '/**/**',
       },
     ],
   },
